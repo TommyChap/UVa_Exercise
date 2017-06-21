ff336df6c2924a46be643db8b7f2e420bf465a6f
TommyChap <towwy321@gmail.com>
2017-06-21 19:30:42 +0800

    20170621193042

--- UVa101/uva101.c
+++ UVa101/uva101.c
@@ -8,9 +8,29 @@ struct Block{
 
 struct Block blocks[25];
 
+short block_equal(short a, short b){
+	short tmp,chk_point;
+	chk_point = 0;
+	tmp = a;
+	while(blocks[tmp].tail != -1){
+		if(blocks[tmp].tail == b) chk_point++;
+		tmp = blocks[tmp].tail;
+	}
+	tmp = b;
+	while(blocks[tmp].tail != -1){
+		if(blocks[tmp].tail == a) chk_point++;
+		tmp = blocks[tmp].tail;
+	}
+	if(chk_point){
+		return 0;
+	}else{
+		return 1;
+	}
+}
+
 void move_onto(short a, short b){
 	short tmp,tmp_tmp;
-	if(a != b){
+	if(block_equal(a,b)){
 		tmp = a;
 		do{
 			tmp_tmp = blocks[tmp].tail;
@@ -32,7 +52,14 @@ void move_onto(short a, short b){
 
 void move_over(short a, short b){
 	short tmp,tmp_tmp;
-	if(a != b){
+	if(block_equal(a,b)){
+		tmp = a;
+		do{
+			tmp_tmp = blocks[tmp].tail;
+			blocks[tmp].tail = -1;
+			tmp = tmp_tmp;
+			blocks[tmp].front = -1;
+		}while(blocks[tmp].tail != -1);
 		tmp = b;
 		if(blocks[tmp].tail != -1){
 			while(blocks[tmp].tail != -1){
@@ -47,8 +74,48 @@ void move_over(short a, short b){
 	}
 }
 
-int main(void){
+void pile_onto(short a, short b){
+	short tmp,tmp_tmp;
+	if(block_equal(a,b)){
+		tmp = a;
+		do{
+			tmp_tmp = blocks[tmp].tail;
+			blocks[tmp].tail = -1;
+			tmp = tmp_tmp;
+			blocks[tmp].front = -1;
+		}while(blocks[tmp].tail != -1);
+		tmp = b;
+		do{
+			tmp_tmp = blocks[tmp].tail;
+			blocks[tmp].tail = -1;
+			tmp = tmp_tmp;
+			blocks[tmp].front = -1;
+		}while(blocks[tmp].tail != -1);
+		blocks[a].front = b;
+		blocks[b].tail = a;
+	}
+}
 
+void pile_over(short a, short b){
+	short tmp,tmp_tmp;
+	if(block_equal(a,b)){
+		tmp = b;
+		if(blocks[tmp].tail != -1){
+			while(blocks[tmp].tail != -1){
+				tmp = blocks[tmp].tail;
+			}
+			blocks[blocks[a].front].tail = -1;
+			blocks[tmp].tail = a;
+			blocks[a].front = tmp;
+		}else{
+			blocks[blocks[a].front].tail = -1;
+			blocks[a].front = b;
+			blocks[b].tail = a;
+		}
+	}
+}
+
+int main(void){
 	int n,sw,i,a,b;
 	short tmp;
 	char cmd[5],cmd2[5];
@@ -60,9 +127,9 @@ int main(void){
 		sw = 1;
 		while(sw){
 			scanf("%s",cmd);
+			scanf("%d%s%d",&a,cmd2,&b);
 			switch(cmd[0]){
 				case 'm':
-					scanf("%d%s%d",&a,cmd2,&b);
 					switch(cmd2[1]){
 						case 'n':
 							move_onto(a,b);
@@ -73,12 +140,12 @@ int main(void){
 					}
 					break;
                                 case 'p':
-					scanf("%d%s%d",&a,cmd2,&b);
 					switch(cmd2[1]){
 						case 'n':
-
+							pile_onto(a,b);
 							break;
 						case 'v':
+							pile_over(a,b);
 							break;
 					}
                                         break;
@@ -86,10 +153,8 @@ int main(void){
 					sw = 0;
 					break;
 			}
-		//}
+		}
 		for(i=0;i<n;i++){
-			/*printf("blocks_front[%d]=%hd\n",i,blocks[i].front);
-			printf("blocks_tail[%d]=%hd\n",i,blocks[i].tail);*/
 			printf("%d:",i);
 			tmp = i;
 			if(blocks[i].front == -1){
@@ -101,7 +166,6 @@ int main(void){
 			}else{
 				printf("\n");
 			}
-		}
 		}
 	}
 	return 0;
